e7b302ca8006133113c8ab23285395cea1be698d
Jamie Yu <jme.kim.yu@gmail.com>
2017-02-10 11:34:08 -0500

    installed redux dev tools

---
+++ browser/react/reducers/root-reducer.js
@@ -0,0 +1,13 @@
+import {SET_LYRICS} from '../constants';
+
+const initialState = {text: ''};
+
+export default function reducer (prevState = initialState, action) {
+    switch(action.type){
+        case 'SET_LYRICS':
+            return Object.assign({}, prevState, {text: action.lyric});
+            break;
+        default:
+            return prevState;
+    }
+}
